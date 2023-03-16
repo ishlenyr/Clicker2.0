b600d8c9f528ea332a0a2c794a91ac1ff364a750
zuzeyka <stepan.tarasenko.26@gmail.com>
2023-03-16 21:05:20 +0200

    connect audio with logic

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ const myGame = new Game();
 
 const settingsDialog = document.getElementById("settings-dialog");
 const openModalBtn = document.getElementById("open-settings");
+if (backMusic.paused) {
+	backMusic.play();
+}
 openModalBtn?.addEventListener("click", () => {
-  settingsDialog?.showModal();
+	settingsDialog?.showModal();
 });
